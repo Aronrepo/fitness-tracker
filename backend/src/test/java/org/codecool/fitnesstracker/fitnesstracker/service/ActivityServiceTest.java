933c9a8ff281261d8fbde9b4874e2263faa02d8a
Bence Fabian <bence.fabian@codecool.com>
2023-08-04 10:52:35 +0200

    use Spring application context for tests

--- backend/src/test/java/org/codecool/fitnesstracker/fitnesstracker/service/ActivityServiceTest.java
+++ backend/src/test/java/org/codecool/fitnesstracker/fitnesstracker/service/ActivityServiceTest.java
@@ -1,20 +1,28 @@
 package org.codecool.fitnesstracker.fitnesstracker.service;
 
-import org.codecool.fitnesstracker.fitnesstracker.controller.dto.ActivityDTO;
-import org.codecool.fitnesstracker.fitnesstracker.controller.dto.NewActivityDTO;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.codecool.fitnesstracker.fitnesstracker.controller.dto.ActivityDTO;
+import org.codecool.fitnesstracker.fitnesstracker.controller.dto.NewActivityDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ActivityServiceTest {
+    ActivityService activityService;
+
+    @Autowired
+    public ActivityServiceTest(ActivityService activityService) {
+        this.activityService = activityService;
+    }
 
     @Test
     void test_getAllActivities() {
-        ActivityService activityService = new ActivityService();
         activityService.addNewActivity(new NewActivityDTO("Running", 2));
         List<ActivityDTO> userActivity = new ArrayList<>();
         userActivity.add(new ActivityDTO("Running", 2, LocalDateTime.now()));
