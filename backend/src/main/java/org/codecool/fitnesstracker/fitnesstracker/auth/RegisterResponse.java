32f5245b1977ab4493d4d219097670c11afe24d5
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-11-01 19:02:24 +0100

    create new RegisterResponse

---
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/auth/RegisterResponse.java
@@ -0,0 +1,14 @@
+package org.codecool.fitnesstracker.fitnesstracker.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterResponse {
+    private String email;
+}
