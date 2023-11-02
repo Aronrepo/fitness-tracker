3b0348dd67afe1ee747c13204e680440c972fdef
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-11-02 16:43:29 +0100

    add new querymethod to get activities

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/repositories/ActivityRepository.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/repositories/ActivityRepository.java
@@ -1,11 +1,15 @@
 package org.codecool.fitnesstracker.fitnesstracker.repositories;
 
 import org.codecool.fitnesstracker.fitnesstracker.dao.model.Activity;
+import org.codecool.fitnesstracker.fitnesstracker.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
 
     List<Activity> findByUserEmail(String userEmail);
+
+    List<Activity> findByUserAndActivityDateTimeAfter(User user, LocalDateTime localDateTime);
 }
