b68df80cb93122f54d1e80d1d2ed5c62ebf90a5b
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-11-04 21:47:19 +0100

    change name to duration to have a better understanding

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/dto/NewActivityDTO.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/dto/NewActivityDTO.java
@@ -2,5 +2,5 @@ package org.codecool.fitnesstracker.fitnesstracker.controller.dto;
 
 import java.time.LocalDateTime;
 
-public record NewActivityDTO(long activityId, int calories) {
+public record NewActivityDTO(long activityId, int duration) {
 }
