2cc9630826bf0d496639299fd9afdc20d1e59fde
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-11-04 23:00:12 +0100

    create a service in order to run the file read at startup

---
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/eventlistener/ApplicationStartupListener.java
@@ -0,0 +1,24 @@
+package org.codecool.fitnesstracker.fitnesstracker.eventlistener;
+
+import lombok.RequiredArgsConstructor;
+import org.codecool.fitnesstracker.fitnesstracker.service.DataInitializationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationStartupListener {
+
+    DataInitializationService dataInitializationService;
+    @Autowired
+    public ApplicationStartupListener(DataInitializationService dataInitializationService) {
+        this.dataInitializationService = dataInitializationService;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        System.out.println("hello world, I have just started up");
+        dataInitializationService.initDataFromJsonFile();
+    }
+}
