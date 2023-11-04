c5f86837fe2ec28c8e76a7f049b820dbe474d0b4
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-11-04 18:09:40 +0100

    modify the AnalyticDailyDTO

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/dto/AnalyticDailyDTO.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/controller/dto/AnalyticDailyDTO.java
@@ -2,5 +2,5 @@ package org.codecool.fitnesstracker.fitnesstracker.controller.dto;
 
 import java.time.LocalDate;
 
-public record AnalyticDailyDTO(int requiredCalorie, int dailyCalorieConsumption, int dailyActivity, LocalDate activityDate) {
+public record AnalyticDailyDTO(int requiredCalorie, double dailyCalorieConsumption, double dailyActivityCalorie, LocalDate activityDate) {
 }
