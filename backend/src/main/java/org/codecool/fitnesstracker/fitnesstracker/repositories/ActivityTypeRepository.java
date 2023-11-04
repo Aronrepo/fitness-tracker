b26747f963c08d4797df42105c73619acc2c19bc
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-11-04 22:05:55 +0100

    add annotation

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/repositories/ActivityTypeRepository.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/repositories/ActivityTypeRepository.java
@@ -2,7 +2,9 @@ package org.codecool.fitnesstracker.fitnesstracker.repositories;
 
 import org.codecool.fitnesstracker.fitnesstracker.dao.model.ActivityType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ActivityTypeRepository extends JpaRepository<ActivityType, Long> {
     ActivityType findActivityTypeById(long activityTypeId);
 }
