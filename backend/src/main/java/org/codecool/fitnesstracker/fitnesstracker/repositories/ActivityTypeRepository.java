d7c41e3afd5f5d72d0d489aa01fe23655caa0721
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-11-06 09:21:53 +0100

    add method query to find activitytype

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/repositories/ActivityTypeRepository.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/repositories/ActivityTypeRepository.java
@@ -4,7 +4,11 @@ import org.codecool.fitnesstracker.fitnesstracker.dao.model.ActivityType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ActivityTypeRepository extends JpaRepository<ActivityType, Long> {
+    List<ActivityType> findActivityTypeByActivityTypeIsLikeIgnoreCase(String activityType);
     ActivityType findActivityTypeById(long activityTypeId);
+
 }
