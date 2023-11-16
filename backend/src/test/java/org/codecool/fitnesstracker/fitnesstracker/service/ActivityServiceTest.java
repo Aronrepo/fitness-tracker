b86de4dfc514ca24ccad489e02981cea766eecbe
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-11-16 10:50:29 +0100

    add unit test for ActivityService getAllActivities method

--- backend/src/test/java/org/codecool/fitnesstracker/fitnesstracker/service/ActivityServiceTest.java
+++ backend/src/test/java/org/codecool/fitnesstracker/fitnesstracker/service/ActivityServiceTest.java
@@ -2,26 +2,61 @@ package org.codecool.fitnesstracker.fitnesstracker.service;
 
 import org.codecool.fitnesstracker.fitnesstracker.controller.dto.ActivityDTO;
 import org.codecool.fitnesstracker.fitnesstracker.controller.dto.NewActivityDTO;
+import org.codecool.fitnesstracker.fitnesstracker.dao.model.Activity;
+import org.codecool.fitnesstracker.fitnesstracker.dao.model.ActivityType;
+import org.codecool.fitnesstracker.fitnesstracker.repositories.ActivityRepository;
+import org.codecool.fitnesstracker.fitnesstracker.repositories.ActivityTypeRepository;
+import org.codecool.fitnesstracker.fitnesstracker.user.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class ActivityServiceTest {
 
+    @Mock
+    private ActivityRepository activityRepository;
+
+    @Mock
+    private ActivityTypeRepository activityTypeRepository;
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private ActivityService activityService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     void test_getAllActivities() {
-//        ActivityService activityService = new ActivityService();
-//        activityService.addNewActivity(new NewActivityDTO("Running", 2));
-//        List<ActivityDTO> userActivity = new ArrayList<>();
-//        userActivity.add(new ActivityDTO("Running", 2, LocalDateTime.now()));
-//
-//        List<ActivityDTO> actual = activityService.getAllActivities();
-//        List<ActivityDTO> expected = userActivity;
-//        assertIterableEquals(expected, actual);
+        String userEmail = "test@example.com";
+        User user = new User();
+        user.setEmail(userEmail);
+
+        List<Activity> mockedActivities = new ArrayList<>();
+        mockedActivities.add(new Activity(new ActivityType(), 30, LocalDateTime.now(), user));
+
+        when(activityRepository.findByUserEmail(userEmail)).thenReturn(mockedActivities);
+
+        List<ActivityDTO> result = activityService.getAllActivities(userEmail);
+
+        assertEquals(1, result.size());
+        assertEquals(30, result.get(0).activityDuration());
+
+        verify(activityRepository, times(1)).findByUserEmail(any(String.class));
+
     }
 
 }
