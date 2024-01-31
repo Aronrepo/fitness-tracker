a7382027cd4bee3205ba7899e37e7f9be545083b
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2024-01-31 11:34:47 +0100

    remove unnecessary imports

--- backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/service/UserService.java
+++ backend/src/main/java/org/codecool/fitnesstracker/fitnesstracker/service/UserService.java
@@ -1,25 +1,15 @@
 package org.codecool.fitnesstracker.fitnesstracker.service;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.codecool.fitnesstracker.fitnesstracker.auth.AuthenticationService;
-import org.codecool.fitnesstracker.fitnesstracker.controller.dto.NewUserDTO;
 import org.codecool.fitnesstracker.fitnesstracker.controller.dto.UserDTO;
 import org.codecool.fitnesstracker.fitnesstracker.controller.dto.UserInfoDTO;
 import org.codecool.fitnesstracker.fitnesstracker.user.User;
-import org.codecool.fitnesstracker.fitnesstracker.exceptions.InvalidCredentialsException;
 import org.codecool.fitnesstracker.fitnesstracker.user.UserRepository;
 import org.codecool.fitnesstracker.fitnesstracker.exceptions.UserNotFoundException;
-import org.codecool.fitnesstracker.fitnesstracker.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
